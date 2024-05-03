14d4530cc9d31679239e79dc888d45cd99be9ebe
Islam Mohammed.ex <Islam.Mohammed.ex@geidea.net>
2024-05-04 00:48:17 +0300

    handle json view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,38 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JSONPath } from 'jsonpath-plus';
 import { DSLUserwithMorethanOneContract, cable, cableConf } from './enum';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'json-path-test';
+
+  fillterValue: any = '';
+  fillterResult: any = 'no data ';
+  jsonData: any;
+
+  cableDataleft: any;
+  cabledataRight: any;
+  cabledataAllMapping: any;
+  dslData: any;
   constractor() {}
   ngOnInit(): void {
     console.log('hello islam');
-    const cableDataleft = JSONPath({
+    this.cableDataleft = JSONPath({
       path: '$[*].userAssets[?(@.entityType=="product")].characteristic[?(@.name=="stack" && @.value=="Cable")]^^^',
       json: cable,
     });
-    const cabledataRight = JSONPath({
+    this.cabledataRight = JSONPath({
       path: '$.relatedAsset[?(@.entityType=="billingAccount")]',
       json: cable,
     });
-    const cabledataAllMapping = JSONPath({
+    this.cabledataAllMapping = JSONPath({
       path: '$[*].userAssets[?(@.entityType=="product")].characteristic[?(@.name=="stack" && @.value=="Cable")]^^^.relatedAsset[?(@.entityType=="billingAccount")]',
       json: cableConf,
     });
-    const dslData = JSONPath({
+    this.dslData = JSONPath({
       path: '$[*].userAssets[?(@.entityType=="product")].characteristic[?(@.name=="stack" && @.value=="DSL")]^^^',
       json: DSLUserwithMorethanOneContract,
     });
+  }
 
-    console.log('cableDataleft', cableDataleft);
-    console.log('cableDataRight', cabledataRight);
-    console.log('cableDataAllMapping', cabledataAllMapping);
-    console.log('dslData', dslData);
+  filterByValue() {
+    try {
+      var parsedData = JSON.parse(this.jsonData);
+      console.log('Parsed JSON data:', parsedData);
+      // Now you can work with the parsed data
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+    }
+    this.fillterResult = JSONPath({
+      path: this.fillterValue,
+      json: parsedData,
+    });
   }
 }
